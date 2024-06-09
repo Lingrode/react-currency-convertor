10e84f613e46ae5617fd98584a9b51f0b0c8e743
Lingrode <grimaloilya@gmail.com>
2024-06-08 22:46:41 -0600

    receiving data from the API and entering values

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import { Block } from './Block';
-import './index.scss';
+import React from "react";
+import { Block } from "./Block";
+import "./index.scss";
 
 function App() {
+  const [fromCurrency, setFromCurrency] = React.useState("UAH");
+  const [toCurrency, setToCurrency] = React.useState("USD");
+  const [fromPrice, setFromPrice] = React.useState(0);
+  const [toPrice, setToPrice] = React.useState(0);
+  const [rates, setRates] = React.useState({});
+
+  React.useEffect(() => {
+    fetch(
+      "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json"
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        setRates(json.rates);
+        console.log(json.rates);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Error");
+      });
+  }, []);
+
+  const onChangeFromPrice = (value) => {
+    setFromPrice(value);
+  };
+
+  const onChangeToPrice = (value) => {
+    setToPrice(value);
+  };
+
   return (
     <div className="App">
-      <Block value={0} currency="UAH" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block
+        value={fromPrice}
+        currency={fromCurrency}
+        onChangeCurrency={setFromCurrency}
+        onChangeValue={onChangeFromPrice}
+      />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency}
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   );
 }
